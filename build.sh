276f219673ddc10903b3940dab65fa41a814d263
shengxiang <hzsunshx@corp.netease.com>
2015-10-09 23:58:35 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+#
+
+if test -n "$TRAVIS"
+then
+	export GOPATH=$HOME/gopath
+fi
+
+GIT_DEPTH=10
+GIT_BRANCH=master
+GIT_REPO="github.com/codeskyblue/forego"
+
+GIT_WORKDIR="$GOPATH/src/$GIT_REPO"
+STORAGE_DIR="/gorelease/codeskyblue/forego/$GIT_BRANCH"
+export STORAGE_DIR
+
+git clone --depth=$GIT_DEPTH --branch=$GIT_BRANCH \
+	"https://$GIT_REPO" "$GIT_WORKDIR"
+
+SELF_DIR=$(cd $(dirname $0); pwd)
+cd "$GIT_WORKDIR"
+bash -x $SELF_DIR/gorelease.sh
+#bash -c "$(curl -fsSL http://bitly.com/gorelease)" gorelease
---
+++ gorelease.sh
@@ -0,0 +1,102 @@
+#!/bin/bash -
+#
+# gorelease.sh: build and publish
+#
+# shorted url: <http://bitly.com/gorelease>
+# need login to view stats: https://bitly.com/a/stats
+#
+# ref:
+# - [travis-ci environment](http://docs.travis-ci.com/user/environment-variables/)
+
+set -e
+set -o pipefail
+
+# FIXME(ssx): only support go1.5 now
+# Set environment variables
+GORELEASE_GO_VERSION="1.5"
+BUILD_OS=${1:-"windows linux darwin"}
+TMPDIR=$PWD/gorelease-temp
+BRANCH=
+
+if test -z "$TRAVIS"
+then
+	# Here for my test
+	GORELEASE_TOKEN=12345678
+	BRANCH=master
+else
+	GORELEASE_TOKEN=${GORELEASE_TOKEN:-"$GRTOKEN"}
+	GORELEASE_TOKEN=${GORELEASE_TOKEN:?}
+	BRANCH=${TRAVIS_BRANCH:-$TRAVIS_TAG}
+fi
+
+KEY_PREFIX=${STORAGE_DIR} 
+if test -z "$KEY_PREFIX"
+then
+	KEY_PREFIX=/gorelease${PWD#*/src/github.com}/${BRANCH:?}/
+fi
+
+echo "BRANCH: $BRANCH"
+echo "PREFIX: $KEY_PREFIX"
+
+if test -n "$TRAVIS" -a "X$TRAVIS_GO_VERSION" != "X$GORELEASE_GO_VERSION"; then
+	echo "Expect go$GORELEASE_GO_VERSION, but travis got go$TRAVIS_GO_VERSION"
+	exit 0
+fi
+
+# Set build environment
+if test -n "$TRAVIS"
+then
+	go get github.com/mitchellh/gox
+	if test $GORELEASE_GO_VERSION != "1.5"
+	then
+		gox -os="${BUILD_OS}" -build-toolchain
+	fi
+	go get github.com/gorelease/qsync
+else
+	BUILD_OS="darwin"
+fi
+
+
+/bin/mkdir -p $TMPDIR
+DISTDIR=$TMPDIR/dist
+
+# FIXME(ssx): need support build pack
+# build standalone
+if test -f .gopack.yml
+then
+    go get github.com/tools/godep
+	go get github.com/gorelease/gopack
+	gopack all \
+		--output "$DISTDIR/{{.OS}}-{{.Arch}}/{{.Dir}}.zip" \
+		--json "$DISTDIR/builds.json"
+else
+	gox -os "$BUILD_OS" -output "$DISTDIR/{{.OS}}-{{.Arch}}/{{.Dir}}"
+cat > $DISTDIR/builds.json <<EOF
+{
+	"update_time": $(date +%s),
+	"go_version": "$GORELEASE_GO_VERSION",
+	"commit": "$TRAVIS_COMMIT"
+}
+EOF
+fi
+
+cat > $TMPDIR/conf.ini <<EOF
+[qiniu]
+uphost = http://up.qiniug.com
+bucket = ""
+accesskey = ""
+secretkey = ""
+keyprefix = $KEY_PREFIX
+
+[local]
+syncdir = $DISTDIR
+
+[gorelease]
+token = "$GORELEASE_TOKEN"
+host = "qntoken.herokuapp.com"
+EOF
+
+set -eu
+
+# upload
+qsync -c $TMPDIR/conf.ini
